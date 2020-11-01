d1fed739db1f3cf474e012a58ac0b38a09650dff
Dwayne Morgan <dwaynerowayne@gmail.com>
2020-11-01 13:09:38 -0500

    I am committing multiple files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 /* Add your JavaScript to this file */
+window.onload = function newsletter(){
+
+	let form=document.getElementsByTagName('form').item(0);
+	form.onsubmit=function newsletter(){
+		return addEmail(this);
+	};
+
+}
+
+function addEmail(formData){
+	let newEmail= formData.email.value;
+	var messageElement=document.querySelector('.message');
+	var message = null;
+
+	if(newEmail){
+		message= "Thank you! Your email Address $(newEmail) has been added to your mailing list";
+	} else{
+		message = "Please enter a valid email";
+	}
+
+	messageElement.innerHTML = message;
+	return false;
+}
